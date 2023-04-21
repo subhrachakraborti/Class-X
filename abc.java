83aac18011bf615065cac030e902b5ca507f8124
Subhra Chakraborti <cers.subhra@gmail.com>
2023-04-21 12:34:32 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,11 @@
+class abc 
+{
+    public static void main(String args[])
+    {
+        Sum ob = new Sum();
+        ob.x=15;
+        ob.y=18;
+        ob.add();
+        ob.display();
+    }
+}
